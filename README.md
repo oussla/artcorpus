20b140831311889f4dac5077a0ee0f8f1c9d1ccb
Nicolas Lagarde <oucelavatil@gmail.com>
2016-03-14 22:31:13 +0100

    First commit: Art Corpus theme, based on Underscores

---
+++ README.md
@@ -0,0 +1,10 @@
+Art Corpus
+==========
+
+Custom Wordpress theme for Art Corpus Tattoo Shop, Paris. 
+
+
+
+---
+* Based on Underscores http://underscores.me/, (C) 2012-2016 Automattic, Inc., [GPLv2 or later](https://www.gnu.org/licenses/gpl-2.0.html)
+* normalize.css http://necolas.github.io/normalize.css/, (C) 2012-2016 Nicolas Gallagher and Jonathan Neal, [MIT](http://opensource.org/licenses/MIT)
---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Art Corpus Theme Customizer.
+ *
+ * @package Art_Corpus
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function artcorpus_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'artcorpus_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function artcorpus_customize_preview_js() {
+	wp_enqueue_script( 'artcorpus_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'artcorpus_customize_preview_js' );
