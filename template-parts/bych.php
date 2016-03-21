881d13bf7076ebb8037c2a36c02784fe5ed82495
Nicolas Lagarde <oucelavatil@gmail.com>
2016-03-21 23:18:44 +0100

    Added BYCH base template

---
+++ template-parts/bych.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template part for displaying the "Before You Come Here" message.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Art_Corpus
+ */
+
+
+?>
+
+YO BYCH.<br />
