06d59d670e3bf4307d7a6d98ac3a1e8f4ff8f277
Nicolas Lagarde <oucelavatil@gmail.com>
2016-07-24 22:33:06 +0200

    Completed new template part, BYCH

--- template-parts/bych.php
+++ template-parts/bych.php
@@ -10,4 +10,14 @@
 
 ?>
 
-YO BYCH.<br />
+<section class="bych">
+
+	<div class="pointing pointing-right"></div>
+
+	<a href="#">
+		<img src="<?php echo get_template_directory_uri(); ?>/img/svg/bych.svg" alt="<?php _("Avant de passer à la boutique", "artcorpus"); ?>" />
+	</a>
+
+	<div class="pointing pointing-left"></div>
+
+</section>
