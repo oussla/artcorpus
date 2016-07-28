a0ad1de8aa491f7fe91565d66ea5bd200ba60bf8
Nicolas Lagarde <oucelavatil@gmail.com>
2016-07-28 23:19:30 +0200

    Added new template part for artists grid

---
+++ template-parts/artists-grid.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template part for displaying artists grid.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Art_Corpus
+ */
+
+
+?>
+
+<section class="artists-grid background-black">
+
+<h3>Tous les artistes</h3>
+
+</section>
