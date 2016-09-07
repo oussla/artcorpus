64386bc392dc8281c8fb05e50f4d20294ce3d70e
Nicolas Lagarde <oucelavatil@gmail.com>
2016-09-07 23:28:08 +0200

    Single posts page, first steps

--- single.php
+++ single.php
@@ -12,20 +12,19 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+		<section class="post-single">
 
-			get_template_part( 'template-parts/content', get_post_format() );
+			<?php
+			while ( have_posts() ) : the_post();
 
-			the_post_navigation();
+				get_template_part( 'template-parts/content', get_post_format() );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				the_post_navigation();
 
-		endwhile; // End of the loop.
-		?>
+			endwhile; // End of the loop.
+			?>
+
+		</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
