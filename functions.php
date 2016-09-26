d2cd660bf6c991bec442d88b067b7d41ccc50c29
Nicolas Lagarde <oucelavatil@gmail.com>
2016-09-26 22:57:42 +0200

    Added new sidebar defintion for Contact page

--- functions.php
+++ functions.php
@@ -125,7 +125,7 @@ function artcorpus_widgets_init() {
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'after_title'   => '</h2>'
 	) );
 
 	register_sidebar( array(
@@ -135,7 +135,18 @@ function artcorpus_widgets_init() {
 		'before_widget' => '<address>',
 		'after_widget'  => '</address>',
 		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'after_title'   => '</h2>'
+	) );
+
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'Contact Sidebar', 'artcorpus' ),
+		'id'            => 'contact-sidebar',
+		'description'   => '',
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>'
 	) );
 
 
