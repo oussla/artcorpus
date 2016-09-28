6b9eada34fc752d4d6777a3f0da3f9df609e331d
Nicolas LAGARDE <n.lagarde@mazarinedigital.com>
2016-09-28 15:25:25 +0200

    Added vinyl-ftp deployment task, to be used with Travis CI - TODO: extract conf

--- _preprocess/gulpfile.js
+++ _preprocess/gulpfile.js
@@ -4,7 +4,12 @@
 
 var gulp = require('gulp'),
 	sass = require('gulp-ruby-sass'),
-	util = require('gulp-util');
+	util = require('gulp-util'),
+	ftp = require('vinyl-ftp'),
+	minimist = require('minimist');
+
+// Get command arguments
+var args = minimist(process.argv.slice(2));
 
 /**
  * SASS task 
@@ -22,4 +27,36 @@ gulp.task('sass', function() {
  */
 gulp.task('watch', function() {
 	gulp.watch('../sass/**/*.scss', ['sass']);
+});
+
+/**
+ * FTP deployment
+ * ###TODO: define options according to target env. 
+ */
+gulp.task('deploy', function() {
+
+	var remotePath = '/www/wip/artcrp/www/wp-content/themes/artcorpus/_preprocess/';
+	var conn = ftp.create({
+		host: 'ftp.nicolaslagarde.com',
+		user: args.user,
+		password: args.password,
+		parallel: 10,
+		log: util.log
+	});
+
+	var globs = [
+        '../img/**',
+        '../inc/**',
+        '../js/**',
+        '../languages/**',
+        '../layouts/**',
+        '../page-template/**',
+        '../template-parts/**',
+        '../*.@(php|txt|js|css|png)'
+    ];
+
+    return gulp.src(globs, {base: '.'})
+        .pipe(conn.newer(remotePath)) // only upload newer files 
+        .pipe(conn.dest(remotePath));
+
 });
