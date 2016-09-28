f51583646ac43cfcd644fe85219922301a483eb4
Nicolas Lagarde <oucelavatil@gmail.com>
2016-09-28 22:41:38 +0200

    Added TinyMCE custom formats selector

--- functions.php
+++ functions.php
@@ -231,6 +231,53 @@ function artcorpus_disable_emojicons_tinymce( $plugins ) {
 }
 
 
+
+/**
+ * TinyMCE custom Formats selector
+ * http://wordpress.stackexchange.com/questions/167685/add-css-class-to-link-in-tinymce-editor
+ */
+
+/**
+ * Callback function to insert 'styleselect' into the $buttons array
+ * @param  Array $buttons TinyMCE buttons
+ * @return Array          Updated array with "styleselect" entry
+ */
+function artcorpus_mce_buttons_2( $buttons ) {
+    array_unshift( $buttons, 'styleselect' );
+    return $buttons;
+}
+add_filter('mce_buttons_2', 'artcorpus_mce_buttons_2');
+
+/**
+ * Callback function to filter the MCE settings
+ * @param  [type] $init_array [description]
+ * @return [type]             [description]
+ */
+function artcorpus_mce_before_init_insert_formats( $init_array ) {  
+    // Define the style_formats array
+    $style_formats = array(  
+        // Each array child is a format with it's own settings
+        array(  
+            'title' => 'Button',  
+            'selector' => 'a',  
+            'classes' => 'button'             
+        ),
+        array(  
+            'title' => 'Highlight Button',  
+            'selector' => 'a',  
+            'classes' => 'button highlight'             
+        )
+    );  
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode( $style_formats );  
+
+    return $init_array;  
+}
+add_filter( 'tiny_mce_before_init', 'artcorpus_mce_before_init_insert_formats' );
+
+
+
+
 /**
  * TEST FILTER 
  * for post overriding post thumbnails
