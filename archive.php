45ec86792dff4a84f75c60ee90070ab1ccd607bb
Nicolas LAGARDE <n.lagarde@mazarinedigital.com>
2016-10-12 14:26:38 +0200

    Double title headers on archive

--- archive.php
+++ archive.php
@@ -17,7 +17,7 @@ get_header(); ?>
 				<?php
 				if ( have_posts() ) : ?>
 
-					<header class="page-header">
+					<header class="page-header double-title">
 						<h2 class="title-checkmarks"><span><?php echo esc_html__('dans les archives', 'artcorpus'); ?></span></h2>
 						<?php
 							the_archive_title( '<h1 class="page-title">', '</h1>' );
