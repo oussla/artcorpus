7aec121914c84de86c0f6a65814ceafa03599aa4
Nicolas Lagarde <oucelavatil@gmail.com>
2016-10-12 22:42:53 +0200

    Layout change on archive page

--- archive.php
+++ archive.php
@@ -14,37 +14,41 @@ get_header(); ?>
 
 			<section class="first posts-list white archives">
 
-				<?php
-				if ( have_posts() ) : ?>
-
-					<header class="page-header double-title">
-						<h2 class="title-checkmarks"><span><?php echo esc_html__('dans les archives', 'artcorpus'); ?></span></h2>
-						<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="taxonomy-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
+				<div class="section-content">
 
 					<?php
-					/* Start the Loop */
-					while ( have_posts() ) : the_post();
+					if ( have_posts() ) : ?>
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', get_post_format() );
+						<header class="page-header double-title">
+							<h2 class="title-checkmarks"><span><?php echo esc_html__('dans les archives', 'artcorpus'); ?></span></h2>
+							<?php
+								the_archive_title( '<h1 class="page-title">', '</h1>' );
+								the_archive_description( '<div class="taxonomy-description">', '</div>' );
+							?>
+						</header><!-- .page-header -->
 
-					endwhile;
+						<?php
+						/* Start the Loop */
+						while ( have_posts() ) : the_post();
 
-					the_posts_navigation();
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', get_post_format() );
 
-				else :
+						endwhile;
 
-					get_template_part( 'template-parts/content', 'none' );
+						the_posts_navigation();
 
-				endif; ?>
+					else :
+
+						get_template_part( 'template-parts/content', 'none' );
+
+					endif; ?>
+
+				</div>
 
 			</section>
 
