9ac17f0cffd083231b6a4693c0ec3585288d9fb6
Nicolas Lagarde <oucelavatil@gmail.com>
2016-10-18 22:23:37 +0200

    Footer menu

--- footer.php
+++ footer.php
@@ -54,11 +54,21 @@
 	 */
 	?>
 	<footer id="colophon" class="site-footer" role="contentinfo">
+
+		<div class="footer-nav">
+			<?php 
+				wp_nav_menu( array( 'theme_location' => 'footer', 
+									'menu_id' => 'footer-menu', 
+									'container' => '', 
+									'menu_class' => 'footer-menu' ) );
+			?>
+		</div>
 		<div class="site-info">			
 			<?php echo __( 'Toutes les images copyright Art Corpus, sauf mention contraire.', 'artcorpus' ); ?>
 
 			<?php printf( esc_html__( 'Theme %1$s by %2$s.', 'artcorpus' ), 'artcorpus', '<a href="http://www.nicolaslagarde.com" rel="designer">Nicolas Lagarde</a>' ); ?>
 		</div><!-- .site-info -->
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
