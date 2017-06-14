732decfecdc2a94ce0fff0924b7d9f265457f70b
Nicolas Lagarde <oucelavatil@gmail.com>
2017-06-14 22:19:45 +0200

    Homepage: added page content

--- page-template/homepage.php
+++ page-template/homepage.php
@@ -68,6 +68,43 @@ get_header(); ?>
 
 		?>
 
+		<section class="homepage-main-content">
+
+			<?php
+				if ( have_posts() ) :
+
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+
+						the_post_thumbnail();
+
+						?>
+						<section class="post-single">
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<div class="entry-content">
+									<?php
+										the_content();
+
+										wp_link_pages( array(
+											'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'artcorpus' ),
+											'after'  => '</div>',
+										) );
+									?>
+								</div><!-- .entry-content -->
+							</article>
+
+						</section>
+
+						<?php
+
+					endwhile;
+
+				endif; 
+			?>
+
+		</section>
+
 		<section class="posts-list white">
 
 			<div class="section-content">
